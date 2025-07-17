6080f4b0893942779c12da543e2374161a736474
Vu Quang Dung <vuquangdung71104@gmail.com>
2025-07-17 11:22:50 +0700

    lesson 9

---
+++ Lesson9/index.php
@@ -0,0 +1,8 @@
+<?php
+include "index.php";
+//alias of Html namespace
+use Project as p;
+$prj = new p\MyClass();
+$prj->myMethod();
+$prj->myFunction();
+?>
---
+++ Lesson9/namespace.php
@@ -0,0 +1,13 @@
+<?php
+namespace MyProject;
+
+class MyClass {
+    public function myMethod() {
+        echo "Hello from MyClass!";
+    }
+    public function myFunction() {
+        echo "Hello from myFunction!";
+    }
+}
+
+?>
